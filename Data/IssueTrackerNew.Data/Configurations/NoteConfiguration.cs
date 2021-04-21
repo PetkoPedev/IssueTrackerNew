75363e66249edc455d90e3bcdea0027331841ccf
Petko Pedev <petko.pedev@gmail.com>
2021-04-21 17:20:30 +0300

    Implement NoteConfiguration to the project.

--- Data/IssueTrackerNew.Data/Configurations/NoteConfiguration.cs
+++ Data/IssueTrackerNew.Data/Configurations/NoteConfiguration.cs
@@ -5,14 +5,21 @@
     using System.Text;
 
     using IssueTrackerNew.Data.Models;
+    using IssueTrackerNew.Data.Validations;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
     public class NoteConfiguration : IEntityTypeConfiguration<Note>
     {
-        public void Configure(EntityTypeBuilder<Note> builder)
+        public void Configure(EntityTypeBuilder<Note> note)
         {
-            // TODO
+            note.Property(n => n.Name)
+                .HasMaxLength(DataValidation.Note.NameMaxLength)
+                .IsRequired();
+
+            note.Property(n => n.Content)
+                .HasMaxLength(DataValidation.Note.ContentMaxLength)
+                .IsRequired();
         }
     }
 }
---
+++ Data/IssueTrackerNew.Data/Migrations/20210421141917_NoteConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IssueTrackerNew.Data.Migrations
+{
+    public partial class NoteConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Notes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Notes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
