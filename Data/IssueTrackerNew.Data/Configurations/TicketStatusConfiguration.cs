13544f496fd30c5d5d2617ef5461d22804e8b23e
Petko Pedev <petko.pedev@gmail.com>
2021-04-21 17:57:40 +0300

    Add TicketStatusConfiguration.cs to project.

---
+++ Data/IssueTrackerNew.Data/Configurations/TicketStatusConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace IssueTrackerNew.Data.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using IssueTrackerNew.Data.Models;
+    using IssueTrackerNew.Data.Validations;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class TicketStatusConfiguration : IEntityTypeConfiguration<TicketStatus>
+    {
+        public void Configure(EntityTypeBuilder<TicketStatus> ticketStatus)
+        {
+            ticketStatus.Property(ts => ts.Name)
+                .HasMaxLength(DataValidation.TicketStatus.TicketStatusNameMaxLength)
+                .IsRequired();
+
+            ticketStatus.HasMany(ts => ts.Tickets)
+                .WithOne(t => t.TicketStatus)
+                .HasForeignKey(ts => ts.TicketStatusId);
+        }
+    }
+}
--- Data/IssueTrackerNew.Data/Migrations/20210421145604_TicketStatusTableUpdate.Designer.cs
+++ Data/IssueTrackerNew.Data/Migrations/20210421145604_TicketStatusTableUpdate.Designer.cs
@@ -4,14 +4,16 @@ using IssueTrackerNew.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IssueTrackerNew.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210421145604_TicketStatusTableUpdate")]
+    partial class TicketStatusTableUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
